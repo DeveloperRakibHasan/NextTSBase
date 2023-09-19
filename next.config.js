95138ba80e2bcbfea927a4a51aece16923eb1763
Rakib Hasan <shihab@pop-os.localdomain>
2023-09-19 14:02:55 +0600

    sass / autoprefixer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config , opts) {
+    if (!opts.isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      }
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{loader: '@svgr/webpack', options: {icon: true}}],
+    })
+    return config
+  },
+}
 
 module.exports = nextConfig
